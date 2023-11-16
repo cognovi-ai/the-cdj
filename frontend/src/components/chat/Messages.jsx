331b0027ba5d5019d7d74b30c8a5cbbb8bb379c5
hiyaryan <meneses.ryan.j@gmail.com>
2023-11-16 01:24:04 -0800

    Display user message on right and llm response on left of chat.

--- frontend/src/components/chat/Messages.jsx
+++ frontend/src/components/chat/Messages.jsx
@@ -1,14 +1,24 @@
-import { useState, useEffect } from "react";
+import React from 'react';
+import { Stack, Grid, Typography } from '@mui/material';
 
 export default function Messages({ chat }) {
     return (
         <div>
             <h2>Chat</h2>
             {chat.messages && chat.messages.map((message, index) => (
-                <div key={index}>
-                    <p>{message.message_content}</p>
-                </div>
+                <Stack container margin="0 0 1em" key={index}>
+                    <Grid item xs={12}>
+                        <Typography align="right" variant="body1">
+                            {message.message_content}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography align="left" variant="body1">
+                            {message.llm_response}
+                        </Typography>
+                    </Grid>
+                </Stack>
             ))}
         </div>
-    )
+    );
 }
