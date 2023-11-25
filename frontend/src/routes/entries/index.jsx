fdd2b376530fc213ed823b3fc26c5143da173c45
hiyaryan <meneses.ryan.j@gmail.com>
2023-11-24 17:08:15 -0800

    Redirect to login if no journalId is found.

--- frontend/src/routes/entries/index.jsx
+++ frontend/src/routes/entries/index.jsx
@@ -1,7 +1,25 @@
 import Entries from '../../components/entries'
 
+import { useEffect } from 'react';
+import { useJournal } from '../../context/useJournal';
+import { useNavigate } from 'react-router-dom';
+
 export default function Index() {
+    const { journalId } = useJournal();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!journalId) {
+            navigate('/login');
+        }
+
+    }, [journalId]);
+
     return (
-        <Entries />
+        <> {journalId && (
+            <Entries />
+        )}
+        </>
     );
 }
