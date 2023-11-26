601f2ddb2ea3812bf8654c730327d41e0be77a75
hiyaryan <meneses.ryan.j@gmail.com>
2023-11-25 23:47:55 -0800

    Setup jest and babel.

---
+++ backend/jest.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    transform: {
+        '^.+\\.[t|j]sx?$': 'babel-jest'
+    },
+    globalSetup: '<rootDir>/tests/jest.setup.cjs',
+    globalTeardown: '<rootDir>/tests/jest.teardown.cjs',
+};
