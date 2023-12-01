1034c6b09a4cbd7f589ab4084b213553ad715021
hiyaryan <meneses.ryan.j@gmail.com>
2023-12-01 14:17:07 -0800

    Add ChatAssistant.
    Add a generic ChatAssistant class.
    Add initialization methods that tests authenticated connections and model availability with unit tests.

---
+++ backend/src/assistants/ChatAssistant.js
@@ -0,0 +1,37 @@
+const baseUrl = 'https://api.openai.com/v1';
+
+export default class ChatAssistant {
+  constructor(bearer, model = '', temperature = 0.7) {
+    this.contentType = 'application/json';
+    this.bearer = `Bearer ${ bearer }`;
+    this.model = model;
+    this.temperature = temperature;
+  }
+
+  /**
+   * Test the connection is authenticated.
+   */
+  async testConnection() {
+    const response = await fetch(
+      baseUrl + '/models',
+      {
+        headers: {
+          Authorization: this.bearer
+        }
+      }
+    );
+
+    return await response.json();
+  }
+
+  /**
+   * Test the model availability.
+   */
+  testModelAvailability(models) {
+    const filteredModel = models.filter((model) =>
+      model.id === this.model.toLowerCase()
+    );
+
+    return filteredModel.length > 0 ? filteredModel : models;
+  }
+}
