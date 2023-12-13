566fff1083a65863282ab7f63ce5c6ed06d37621
hiyaryan <meneses.ryan.j@gmail.com>
2023-12-12 17:53:48 -0800

    Remove non-existent static image and alt.

--- frontend/src/components/nav/menus/UserMenu.jsx
+++ frontend/src/components/nav/menus/UserMenu.jsx
@@ -11,7 +11,7 @@ export default function UserMenu({
         <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                    <Avatar />
                 </IconButton>
             </Tooltip>
             <Menu
