269756ed676f2c825547bf974ae581b6deee99ab
hiyaryan <meneses.ryan.j@gmail.com>
2023-12-13 20:13:42 -0800

    Allow user to click on thought to edit.
    Click on thought to create a TextField with previous thought data.
    Press escape to exit editing mode and reset TextField to previous thought data.
    Click out of the TextField to escape editing mode and preserve data in TextField.
    Press enter to submit edited thought.
    Add validaiton to ensure TextField contains content before submitting.
    Add a linear progress bar to indicate the server is processing the edit request.

--- frontend/src/components/entries/analysis/Analysis.jsx
+++ frontend/src/components/entries/analysis/Analysis.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, LinearProgress, Tooltip, Typography } from '@mui/material';
+import { Box, Button, Grid, LinearProgress, TextField, Tooltip, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 
 import Chat from './chat/Chat';
@@ -9,7 +9,11 @@ import { useEntries } from '../../../hooks/useEntries';
 
 export default function Analysis({ journalId, focusedEntryId, editedEntryId, setAllEntries }) {
     const [focusedData, setFocusedData] = useState({});
-    const [updating, setUpdating] = useState(false);
+    const [editedData, setEditedData] = useState('');
+    const [editing, setEditing] = useState(false);
+    const [isSaving, setIsSaving] = useState(false);
+    const [validationError, setValidationError] = useState('');
+    const [regenerating, setRegenerating] = useState(false);
     const [chat, setChat] = useState({});
 
     const entries = useEntries();
@@ -26,6 +30,7 @@ export default function Analysis({ journalId, focusedEntryId, editedEntryId, set
                 const entryAnalysisData = await entries(`/${ focusedEntryId }/analysis`, 'GET');
 
                 setFocusedData(entryAnalysisData);
+                setEditedData(entryAnalysisData.entry.content);
 
                 const chatData = await entries(`/${ focusedEntryId }/chat`, 'GET');
 
@@ -39,11 +44,75 @@ export default function Analysis({ journalId, focusedEntryId, editedEntryId, set
         makeRequest();
     }, [focusedEntryId, editedEntryId]);
 
+    const handleSave = () => {
+        if (!editedData.length) {
+            setValidationError('This field is required.');
+            return;
+        } else if (editedData === focusedData?.entry?.content) {
+            setValidationError('');
+            setEditing(false);
+            return;
+        }
+
+        setIsSaving(true);
+
+        const makeRequest = async () => {
+            try {
+                // Update the entry on the server
+                await entries(
+                    `/${ focusedEntryId }`,
+                    'PUT',
+                    { 'Content-Type': 'application/json' },
+                    { content: editedData }
+                );
+
+                // Get the entry analysis data
+                const entryAnalysisData = await entries(`/${ focusedEntryId }/analysis`, 'GET');
+
+                // Re-render this component
+                setFocusedData(entryAnalysisData);
+
+                // Side-effect re-renders sibling Thoughts component
+                setAllEntries(allEntries => allEntries.map(entry =>
+                    entry._id === focusedEntryId ? { ...entryAnalysisData.entry } : entry
+                ));
+
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setEditing(false);
+                setIsSaving(false);
+            }
+        }
+
+        makeRequest();
+    }
+
+    const handleEditing = (event) => {
+        // Update the edited data
+        setEditedData(event.target.value);
+    }
+
+    const handleOnBlur = () => {
+        setEditing(false);
+    }
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            handleSave();
+        } else if (event.key === 'Escape') {
+            setEditedData(focusedData?.entry?.content);
+            setValidationError('');
+            setEditing(false);
+        }
+    }
+
     const handleNewAnalysis = (event) => {
         event.preventDefault();
 
         const makeRequest = async () => {
-            setUpdating(true);
+            setRegenerating(true);
 
             try {
                 const response = await entries(`/${ focusedEntryId }/analysis`, 'PUT');
@@ -60,7 +129,7 @@ export default function Analysis({ journalId, focusedEntryId, editedEntryId, set
             } catch (error) {
                 console.error(error);
             } finally {
-                setUpdating(false);
+                setRegenerating(false);
             }
         };
 
@@ -75,12 +144,36 @@ export default function Analysis({ journalId, focusedEntryId, editedEntryId, set
                 </Typography>
                 <Grid container spacing={2}>
                     <Grid item md={6} xs={12}>
-                        <Typography variant="body1">
-                            {focusedData?.entry?.content}
-                        </Typography>
+                        {editing ? (
+                            <TextField
+                                autoFocus
+                                disabled={isSaving}
+                                error={Boolean(validationError)}
+                                fullWidth
+                                helperText={validationError}
+                                label="Edit your thought."
+                                multiline
+                                onBlur={handleOnBlur}
+                                onChange={handleEditing}
+                                onKeyDown={handleKeyPress}
+                                value={editedData}
+                                variant="outlined"
+                            />
+                        ) : (
+                            <Tooltip title="Click to edit your thought.">
+                                <Typography
+                                    onClick={() => setEditing(true)}
+                                    sx={{ cursor: 'pointer' }}
+                                    variant="body1"
+                                >
+                                    {focusedData?.entry?.content}
+                                </Typography>
+                            </Tooltip>
+                        )}
+                        {isSaving && <LinearProgress />}
                     </Grid>
                     <Grid item md={6} xs={12}>
-                        {updating ? (
+                        {regenerating ? (
                             <>
                                 <Typography
                                     fontStyle={'italic'}
@@ -96,10 +189,10 @@ export default function Analysis({ journalId, focusedEntryId, editedEntryId, set
                             </Typography>
                         )
                         }
-                        <Tooltip title="Generate a new analysis">
+                        <Tooltip title="Generate a new analysis.">
                             <Box display="flex" justifyContent="flex-end">
                                 <Button
-                                    disabled={updating}
+                                    disabled={regenerating}
                                 >
                                     <UpdateIcon
                                         fontSize="small"
