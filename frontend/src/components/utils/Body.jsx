e0316f7dc9c85474e8f02a15a7ae1d494fb7c6ce
hiyaryan <meneses.ryan.j@gmail.com>
2023-12-14 23:47:21 -0800

    Remove Body styled component.
    Move Home styles to parent App.css.

--- frontend/src/components/utils/Body.jsx
+++
@@ -1,8 +0,0 @@
-import { Paper } from '@mui/material';
-import { styled } from '@mui/material/styles';
-
-export const Body = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#282828' : 'transparent',
-    padding: '2em',
-    boxShadow: 'none',
-}));
