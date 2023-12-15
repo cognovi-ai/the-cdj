03e9cb3c27acbfef0780d09df05ea5d368ab79db
hiyaryan <meneses.ryan.j@gmail.com>
2023-12-14 23:40:11 -0800

    Refactor createTheme into separate Theme file.
    Rename body class in App.css to outlet indicating styles added to all router components.
    Move background image styles to index.css.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,72 +1,15 @@
 import './App.css'
 
 import { Box, CssBaseline } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import FlashMessages from './components/utils/FlashMessages.jsx';
-
 import Footer from './components/body/Footer';
 import Header from './components/body/Header';
 
 import { JournalProvider } from './context/JournalProvider.jsx';
 import { Outlet } from 'react-router-dom';
-
-const theme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#282828',
-    },
-    chat: {
-      main: '#4fc3f7',
-    },
-    edit: {
-      main: '#4fc3f7',
-    },
-    cancel: {
-      main: '#f44336',
-      contrastText: '#fff',
-    },
-    danger: {
-      main: '#e63939',
-    },
-  },
-  typography: {
-    fontFamily: 'Roboto, sans-serif',
-    fontSize: 16,
-    h1: {
-      fontSize: '2rem',
-      fontWeight: 700,
-      lineHeight: 2.0,
-    },
-    h2: {
-      fontSize: '1.5rem',
-      fontWeight: 600,
-      lineHeight: 3.0,
-    },
-    h3: {
-      fontSize: '1rem',
-      fontWeight: 500,
-      lineHeight: 4.0,
-    },
-    body1: {
-      fontSize: '1rem',
-      fontWeight: 400,
-      lineHeight: 2.0,
-    },
-    body2: {
-      fontSize: '1rem',
-      fontWeight: 300,
-      lineHeight: 2.0,
-    },
-    caption: {
-      fontSize: '0.8rem',
-      fontWeight: 100,
-      lineHeight: 2.0,
-    },
-  },
-  // ...other theme configuration options
-});
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './styles/Theme.jsx';
 
 export default function App() {
   return (
@@ -75,8 +18,8 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <JournalProvider>
           <Header />
-          <FlashMessages />
-          <Box sx={{ minHeight: '100vh' }}>
+          <Box className="outlet" >
+            <FlashMessages />
             <Outlet />
           </Box>
         </JournalProvider>
