858b7eb9a5432eb0fd826d0e3183de31651d78b5
hiyaryan <meneses.ryan.j@gmail.com>
2023-12-17 20:06:52 -0800

    Move theme definition into separate file.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
-
 import { Box, CssBaseline } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import FlashMessages from './components/utils/FlashMessages.jsx';
 
@@ -10,63 +8,8 @@ import Header from './components/body/Header';
 
 import { JournalProvider } from './context/JournalProvider.jsx';
 import { Outlet } from 'react-router-dom';
-
-const theme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#282828',
-    },
-    chat: {
-      main: '#4fc3f7',
-    },
-    edit: {
-      main: '#4fc3f7',
-    },
-    cancel: {
-      main: '#f44336',
-      contrastText: '#fff',
-    },
-    danger: {
-      main: '#e63939',
-    },
-  },
-  typography: {
-    fontFamily: 'Roboto, sans-serif',
-    fontSize: 16,
-    h1: {
-      fontSize: '2rem',
-      fontWeight: 700,
-      lineHeight: 2.0,
-    },
-    h2: {
-      fontSize: '1.5rem',
-      fontWeight: 600,
-      lineHeight: 3.0,
-    },
-    h3: {
-      fontSize: '1rem',
-      fontWeight: 500,
-      lineHeight: 4.0,
-    },
-    body1: {
-      fontSize: '1rem',
-      fontWeight: 400,
-      lineHeight: 2.0,
-    },
-    body2: {
-      fontSize: '1rem',
-      fontWeight: 300,
-      lineHeight: 2.0,
-    },
-    caption: {
-      fontSize: '0.8rem',
-      fontWeight: 100,
-      lineHeight: 2.0,
-    },
-  },
-  // ...other theme configuration options
-});
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './styles/theme.jsx'
 
 export default function App() {
   return (
