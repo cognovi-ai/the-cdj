95b8ec6b68a844a91bb1bdab1368c27b479d4a97
hiyaryan <meneses.ryan.j@gmail.com>
2024-01-06 14:37:57 -0800

    Make chat message box always shows the most recent message.
    Adds a reference to the chat box with a scrollTop property set to the scrollHeight so that recent messages appear first on entry focus and when new messages are submitted.

--- frontend/src/components/entries/analysis/chat/Chat.jsx
+++ frontend/src/components/entries/analysis/chat/Chat.jsx
@@ -1,19 +1,39 @@
 import { Box, InputLabel, Typography } from '@mui/material';
 
+import { useEffect, useRef } from 'react';
+
 import ChatEntry from './ChatEntry';
 import Messages from './Messages';
 
 export default function Chat({ journalId, focusedEntryId, chat, setChat }) {
+    const messagesEndRef = useRef(null);
+
+    useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
+        }
+    }, [chat]);
+
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <InputLabel htmlFor="new-message" sx={{ color: 'inherit' }}>
+            <InputLabel
+                htmlFor="new-message"
+                sx={{ color: 'inherit' }}>
                 <Typography variant="h2">Chat</Typography>
             </InputLabel >
-            {chat.messages && <Box sx={{ overflowY: 'auto', overflowX: 'hidden', flexGrow: 1, maxHeight: '50vh', pl: '1.6em', pr: '1.6em' }}>
-                <Messages
-                    chat={chat}
-                />
-            </Box>}
+            {chat.messages &&
+                <Box
+                    ref={messagesEndRef}
+                    sx={{
+                        overflowY: 'auto',
+                        overflowX: 'hidden',
+                        flexGrow: 1,
+                        maxHeight: '50vh',
+                        pl: '1.6em',
+                        pr: '1.6em'
+                    }}>
+                    <Messages chat={chat} />
+                </Box>}
             <ChatEntry
                 chat={chat}
                 focusedEntryId={focusedEntryId}
--- frontend/src/components/entries/analysis/chat/Messages.jsx
+++ frontend/src/components/entries/analysis/chat/Messages.jsx
@@ -6,21 +6,36 @@ export default function Messages({ chat }) {
     return (
         <Box sx={{ width: '100%' }}>
             {chat && chat.messages && chat.messages.map((message, index) => (
-                <Grid container key={index} margin="0 0 1em" spacing={1}>
+                <Grid
+                    container
+                    key={index}
+                    margin="0 0 1em"
+                    spacing={1}
+                >
                     <Grid item xs={4} />
                     <Grid item style={{ textAlign: 'left' }} xs={8}>
-                        <Typography className="message-bubble blue-bg" variant="body2">
+                        <Typography
+                            className="message-bubble blue-bg"
+                            variant="body2"
+                        >
                             {message.message_content}
                         </Typography>
                     </Grid>
                     <Grid item style={{ textAlign: 'left' }} xs={8}>
-                        <Typography className="message-bubble gray-bg" variant="body2">
+                        <Typography
+                            className="message-bubble gray-bg"
+                            variant="body2"
+                        >
                             {message.llm_response}
                         </Typography>
                     </Grid>
                     <Grid item xs={4} />
                     <Grid item xs={12}>
-                        <Typography align="center" display="block" variant="caption">
+                        <Typography
+                            align="center"
+                            display="block"
+                            variant="caption"
+                        >
                             {message.created_at}
                         </Typography>
                     </Grid>
