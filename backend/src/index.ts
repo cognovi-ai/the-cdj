f595f0e0df304909d6be0c59874a50b285bebbf4
Ryan James Meneses <github@ryanmeneses.com>
2024-07-08 19:12:40 -0700

    Initial TypeScript migration
    Adds the tsconfig.json file.
    Moves and renames index.js to src/index.ts.
    Updates package.json dev script to use tsx with hot reload.
    Adds build, start, and start:prod scripts to package.json.
    Adds pm2, tsx, and typescript to dev dependencies.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import app from './src/app.js';
+import app from './app.js';
 import fs from 'fs';
 import https from 'https';
 
@@ -12,17 +12,17 @@ const port = process.env.PORT;
 
 if (process.env.NODE_ENV !== 'production') {
   app.listen(port, () => {
-    console.log(`\nEXPRESS Listening on port ${ port }`);
+    console.log(`\nExpress listening on port ${ port }`);
   });
 } else {
-  const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_PATH, 'utf8');
-  const certificate = fs.readFileSync(process.env.CERTIFICATE_PATH, 'utf8');
-  const ca = fs.readFileSync(process.env.CA_PATH, 'utf8');
+  const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_PATH || '', 'utf8');
+  const certificate = fs.readFileSync(process.env.CERTIFICATE_PATH || '', 'utf8');
+  const ca = fs.readFileSync(process.env.CA_PATH || '', 'utf8');
 
   const credentials = { key: privateKey, cert: certificate, ca };
   const httpsServer = https.createServer(credentials, app);
 
   httpsServer.listen(port, () => {
-    console.log(`\nHTTPS Listening on port ${ port }`);
+    console.log(`\nHTTPS listening on port ${ port }`);
   });
 }
