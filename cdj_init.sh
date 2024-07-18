1a049942c4a15d533a18d2debddffa86f1d8d980
David Lu <ludavid33@gmail.com>
2024-07-18 16:55:03 -0700

    [150-containerize-the-cdj] Init cdj with script
    Rewrote cdj_init.sh to initialize app with single shell command.

--- cdj_init.sh
+++ cdj_init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+docker compose up -d mongo
+echo "Waiting for mongodb container to start"
+sleep 10
+
+echo "Initializing replica set"
 docker exec -it mongo-cdj mongosh --eval "rs.initiate({
  \"_id\": \"rs0\",
  members: [
@@ -7,4 +12,8 @@ docker exec -it mongo-cdj mongosh --eval "rs.initiate({
  ]
 })"
 
+docker compose up -d --build
 docker exec -it backend-cdj node data/seed.js
+
+docker compose down
+echo "Completed initializing the CDJ"
