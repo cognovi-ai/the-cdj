be2656f91465c7995032e7c5050b6efc54e30f7c
David Lu <ludavid33@gmail.com>
2024-08-09 11:59:30 -0700

    Rewrite access.ts test with supertest (#139)
    Changed unit tests for access.ts to use supertest and check for path
    existence, i.e. non-404 status code. Previous unit tests didn't
    work well with typescript due to mismatch between express router
    type definition and actual object.

--- backend/tests/routes/access/access.test.ts
+++ backend/tests/routes/access/access.test.ts
@@ -1,24 +1,75 @@
 /**
  * @jest-environment node
  */
-import router from '../../../src/routes/access/access';
+import request from 'supertest';
+import app from '../../../src/app';
+jest.mock('../../../src/controllers/access/access.js');
 
-describe('access router', () => {
-  const routes = [
-    { path: '/journal/:journalId', method: 'put' },
-    { path: '/login', method: 'post' },
-    { path: '/token-login', method: 'post' },
-    { path: '/forgot-password', method: 'post' },
-    { path: '/reset-password', method: 'post' },
-    { path: '/logout', method: 'get' },
-    { path: '/register', method: 'post' },
-    { path: '/verify-email', method: 'post' },
-    { path: '/beta-approval', method: 'get' },
-    { path: '/beta-denial', method: 'get' },
-  ];
+describe('access router for /access paths', () => {
+  const testId = 'testId';
 
-  it.each(routes)('should have all routes defined in access.ts', (route) => {
-    expect(router.stack.some((s) => s.route.path === route.path)).toBe(true);
-    expect(router.stack.some((s) => Object.keys(s.route.methods).includes(route.method))).toBe(true);
+  it('should have /access/journal/:journalId PUT route', async () => {
+    const response = await request(app).put(`/access/journal/${testId}`).send('{}');
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it('should have /access/:journalId/account GET route', async () => {
+    const response = await request(app).get(`/access/${testId}/account`).send('{}');
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it('should have /access/:journalId/account PUT route', async () => {
+    const response = await request(app).put(`/access/${testId}/account`).send('{}');
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it('should have /access/:journalId/account DELETE route', async () => {
+    const response = await request(app).delete(`/access/${testId}/account`).send('{}');
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it('should have /access/login POST route', async () => {
+    const response = await request(app).post(`/access/login`).send('{}');
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it('should have /access/token-login POST route', async () => {
+    const response = await request(app).post(`/access/token-login`).send('{}');
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it('should have /access/forgot-password POST route', async () => {
+    const response = await request(app).post(`/access/forgot-password`).send('{}');
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it('should have /access/reset-password POST route', async () => {
+    const response = await request(app).post(`/access/reset-password`).send('{}');
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it('should have /access/logout GET route', async () => {
+    const response = await request(app).get(`/access/logout`);
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it('should have /access/register POST route', async () => {
+    const response = await request(app).post(`/access/register`).send('{}');
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it('should have /access/verify-email POST route', async () => {
+    const response = await request(app).post(`/access/verify-email`).send('{}');
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it('should have /access/beta-approval GET route', async () => {
+    const response = await request(app).get(`/access/beta-approval`);
+    expect(response.statusCode).not.toBe(404);
+  });
+
+  it('should have /access/beta-denial GET route', async () => {
+    const response = await request(app).get(`/access/beta-denial`);
+    expect(response.statusCode).not.toBe(404);
   });
 });
