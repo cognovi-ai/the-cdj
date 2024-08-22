62d7c4fa5270135df521270ca146f80dc6ac7b4b
David Lu <ludavid33@gmail.com>
2024-08-22 13:12:23 -0700

    Run linter (#139)

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   },
   // https://github.com/kulshekhar/ts-jest/issues/1057#issuecomment-1455530737
   moduleNameMapper: {
-    "^(\.\.?\/.+)\.jsx?$": "$1"
+    "^(\\.\\.?\\/.+)\\.jsx?$": "$1"
   },
   globalSetup: "<rootDir>/tests/jest.setup.cjs",
   globalTeardown: "<rootDir>/tests/jest.teardown.cjs"
--- backend/tests/routes/access/access.test.ts
+++ backend/tests/routes/access/access.test.ts
@@ -29,47 +29,47 @@ describe('access router for /access paths', () => {
   });
 
   it('should have /access/login POST route', async () => {
-    const response = await request(app).post(`/access/login`).send('{}');
+    const response = await request(app).post('/access/login').send('{}');
     expect(response.statusCode).not.toBe(404);
   });
 
   it('should have /access/token-login POST route', async () => {
-    const response = await request(app).post(`/access/token-login`).send('{}');
+    const response = await request(app).post('/access/token-login').send('{}');
     expect(response.statusCode).not.toBe(404);
   });
 
   it('should have /access/forgot-password POST route', async () => {
-    const response = await request(app).post(`/access/forgot-password`).send('{}');
+    const response = await request(app).post('/access/forgot-password').send('{}');
     expect(response.statusCode).not.toBe(404);
   });
 
   it('should have /access/reset-password POST route', async () => {
-    const response = await request(app).post(`/access/reset-password`).send('{}');
+    const response = await request(app).post('/access/reset-password').send('{}');
     expect(response.statusCode).not.toBe(404);
   });
 
   it('should have /access/logout GET route', async () => {
-    const response = await request(app).get(`/access/logout`);
+    const response = await request(app).get('/access/logout');
     expect(response.statusCode).not.toBe(404);
   });
 
   it('should have /access/register POST route', async () => {
-    const response = await request(app).post(`/access/register`).send('{}');
+    const response = await request(app).post('/access/register').send('{}');
     expect(response.statusCode).not.toBe(404);
   });
 
   it('should have /access/verify-email POST route', async () => {
-    const response = await request(app).post(`/access/verify-email`).send('{}');
+    const response = await request(app).post('/access/verify-email').send('{}');
     expect(response.statusCode).not.toBe(404);
   });
 
   it('should have /access/beta-approval GET route', async () => {
-    const response = await request(app).get(`/access/beta-approval`);
+    const response = await request(app).get('/access/beta-approval');
     expect(response.statusCode).not.toBe(404);
   });
 
   it('should have /access/beta-denial GET route', async () => {
-    const response = await request(app).get(`/access/beta-denial`);
+    const response = await request(app).get('/access/beta-denial');
     expect(response.statusCode).not.toBe(404);
   });
 });
