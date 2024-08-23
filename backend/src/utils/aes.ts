b824e40e7b169c271889962545448c7e9be894c5
David Lu <ludavid33@gmail.com>
2024-08-23 15:28:34 -0700

    Migrate utils to TS (#139)
    This commit breaks all unit tests due to an issue with the jest
    gloablsetup and globalteardown scripts. The module imports in them seem
    to happen outside of code transformation, so the ExpressError file
    can't be found.

--- backend/src/utils/aes.ts
+++ backend/src/utils/aes.ts
@@ -3,14 +3,12 @@
  */
 
 import crypto from 'crypto';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import 'dotenv/config';
 
 const { ENCRYPTION_KEY } = process.env;
 const IV_LENGTH = 16;
 
-export function encrypt(text) {
+export function encrypt(text: string) {
   if (!ENCRYPTION_KEY) {
     throw new Error('Encryption key is not set!');
   }
@@ -24,13 +22,13 @@ export function encrypt(text) {
   return iv.toString('hex') + ':' + encrypted.toString('hex');
 }
 
-export function decrypt(text) {
+export function decrypt(text: string) {
   if (!ENCRYPTION_KEY) {
     throw new Error('Encryption key is not set!');
   }
 
-  const textParts = text.split(':');
-  const iv = Buffer.from(textParts.shift(), 'hex');
+  const textParts: string[] = text.split(':');
+  const iv = Buffer.from(textParts.shift() || '', 'hex');
   const encryptedText = Buffer.from(textParts.join(':'), 'hex');
   const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
   let decrypted = decipher.update(encryptedText);
