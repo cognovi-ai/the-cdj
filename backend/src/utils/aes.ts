070bbd6868745ec4b9e4245822fecfcda0ae24d5
David Lu <ludavid33@gmail.com>
2024-08-24 14:26:28 -0700

    Unit test utils/aes.ts (#139)

--- backend/src/utils/aes.ts
+++ backend/src/utils/aes.ts
@@ -5,16 +5,15 @@
 import crypto from 'crypto';
 import 'dotenv/config';
 
-const { ENCRYPTION_KEY } = process.env;
 const IV_LENGTH = 16;
 
 export function encrypt(text: string) {
-  if (!ENCRYPTION_KEY) {
+  if (!process.env.ENCRYPTION_KEY) {
     throw new Error('Encryption key is not set!');
   }
 
   const iv = crypto.randomBytes(IV_LENGTH);
-  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(process.env.ENCRYPTION_KEY), iv);
   let encrypted = cipher.update(text);
 
   encrypted = Buffer.concat([encrypted, cipher.final()]);
@@ -23,14 +22,14 @@ export function encrypt(text: string) {
 }
 
 export function decrypt(text: string) {
-  if (!ENCRYPTION_KEY) {
+  if (!process.env.ENCRYPTION_KEY) {
     throw new Error('Encryption key is not set!');
   }
 
   const textParts: string[] = text.split(':');
   const iv = Buffer.from(textParts.shift() || '', 'hex');
   const encryptedText = Buffer.from(textParts.join(':'), 'hex');
-  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(process.env.ENCRYPTION_KEY), iv);
   let decrypted = decipher.update(encryptedText);
 
   decrypted = Buffer.concat([decrypted, decipher.final()]);
