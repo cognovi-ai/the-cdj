5fb9fe60c78390ce37314a8edba99deb43271ea5
Ryan James Meneses <58452495+hiyaryan@users.noreply.github.com>
2024-09-06 09:23:24 -0700

    Migrate db.js to TS with unit tests. (#159)

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,6 +1,6 @@
 import { connect } from 'mongoose';
 
-export default async function connectDB(dbName) {
+export default async function connectDB(dbName: string): Promise<void> {
   // Define the local URI for development and testing
   const localUri = `${process.env.MONGO_URI}/${dbName}`;
 
@@ -10,10 +10,20 @@ export default async function connectDB(dbName) {
   try {
     if (process.env.NODE_ENV === 'production') {
       // Connect to MongoDB Atlas in production
+      if (!atlasUri) {
+        throw new Error(
+          'ATLAS_URI is not defined in the environment variables'
+        );
+      }
       await connect(atlasUri);
       console.log('Connected to MongoDB Atlas');
     } else if (process.env.NODE_ENV !== 'test') {
       // Connect to the local MongoDB for development
+      if (!process.env.MONGO_URI) {
+        throw new Error(
+          'MONGO_URI is not defined in the environment variables'
+        );
+      }
       await connect(localUri);
       console.log(`Connected to local MongoDB: ${localUri}`);
     } else {
