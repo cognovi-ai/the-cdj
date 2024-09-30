84c7e5c2cd96c9800e6aa85e218d4f498a4c24cc
David Lu <ludavid33@gmail.com>
2024-09-30 15:16:00 -0700

    refactor!: Migrate Journal model to TS

---
+++ backend/src/models/journal.ts
@@ -0,0 +1,45 @@
+import { Schema, model, Types, Model } from 'mongoose';
+import Joi from 'joi';
+
+export interface JournalType {
+  user: Types.ObjectId,
+  config?: Types.ObjectId,
+  title?: string,
+  created_at?: Date,
+  updated_at?: Date,
+}
+
+interface JournalStatics extends Model<JournalType> {
+  joi(obj: any, options: object): Joi.ValidationResult<any>,
+}
+
+const journalSchema = new Schema<JournalType, JournalStatics>({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  config: { type: Schema.Types.ObjectId, ref: 'Config' },
+  title: { type: String, default: 'The Cognitive Distortion Journal' },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now }
+});
+
+journalSchema.statics.joi = function (obj: any, options: object): Joi.ValidationResult<any> {
+  const journalJoiSchema = Joi.object({
+    title: Joi.string()
+      .max(100)
+      .trim()
+      .default('The Cognitive Distortion Journal')
+  });
+  return journalJoiSchema.validate(obj, options);
+}
+
+// Indexing on user as journals will often be queried per user.
+journalSchema.index({ user: 1 });
+// If journals are listed by creation date.
+journalSchema.index({ created_at: -1 });
+
+// Set new updated_at value before saving
+journalSchema.pre('save', function (next) {
+  this.set({ updated_at: Date.now() });
+  next();
+});
+
+export default model<JournalType, JournalStatics>('Journal', journalSchema);
