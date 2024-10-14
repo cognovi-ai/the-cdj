4ff5571005579eff8028a0f9f52a4e2318b336bb
David Lu <ludavid33@gmail.com>
2024-10-13 20:19:09 -0700

    lint: fix linter errors

--- backend/tests/models/journal.test.ts
+++ backend/tests/models/journal.test.ts
@@ -3,49 +3,49 @@
  */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { Journal } from "../../src/models/index.js";
+import { Journal } from '../../src/models/index.js';
 
 describe('Journal model unit tests', () => {
   const positiveTestCases = [
     {
       desc: 'title contains valid characters',
       input: {
-        title: 'This title is fine%5843./,;//""'
+        title: 'This title is fine%5843./,;//""',
       },
       expected: {
-        title: 'This title is fine%5843./,;//""'
-      }
+        title: 'This title is fine%5843./,;//""',
+      },
     },
     {
       desc: 'undefined title produces default value',
       input: {},
       expected: {
-        title: 'The Cognitive Distortion Journal'
-      }
+        title: 'The Cognitive Distortion Journal',
+      },
     },
     {
       desc: 'title at most 100 characters',
       input: {
-        title: 'g'.repeat(100)
+        title: 'g'.repeat(100),
       },
       expected: {
-        title: 'g'.repeat(100)
-      }
+        title: 'g'.repeat(100),
+      },
     },
     {
       desc: 'title at most 100 characters with whitespace trimmed',
       input: {
-        title: 'g'.repeat(100).padStart(5, ' ').padEnd(5, ' ')
+        title: 'g'.repeat(100).padStart(5, ' ').padEnd(5, ' '),
       },
       expected: {
-        title: 'g'.repeat(100).padStart(5, ' ').padEnd(5, ' ')
-      }
+        title: 'g'.repeat(100).padStart(5, ' ').padEnd(5, ' '),
+      },
     },
   ];
 
   it.each(positiveTestCases)(
     'joi validation succeeds when $desc',
-    ({ desc, input, expected }) => {
+    ({ input, expected }) => {
       const { error, value } = Journal.joi(input);
 
       expect(error).toBeUndefined();
@@ -57,48 +57,48 @@ describe('Journal model unit tests', () => {
     {
       desc: 'title is not a string',
       input: {
-        title: 123
+        title: 123,
       },
       expected: {
-        title: 123
-      }
+        title: 123,
+      },
     },
     {
       desc: 'title over 100 characters',
       input: {
-        title: 'g'.repeat(101)
+        title: 'g'.repeat(101),
       },
       expected: {
-        title: 'g'.repeat(101)
-      }
+        title: 'g'.repeat(101),
+      },
     },
     {
       desc: 'title is empty string',
       input: {
-        title: ''
+        title: '',
       },
       expected: {
-        title: ''
-      }
+        title: '',
+      },
     },
     {
       desc: 'any other property except title is passed in',
       input: {
         title: 'The Cognitive Distortion Journal',
         created_at: new Date(0),
-        updated_at: new Date(0)
+        updated_at: new Date(0),
       },
       expected: {
         title: 'The Cognitive Distortion Journal',
         created_at: new Date(0),
-        updated_at: new Date(0)
-      }
+        updated_at: new Date(0),
+      },
     },
   ];
 
   it.each(negativeTestCases)(
     'joi validation fails when $desc',
-    ({ desc, input, expected }) => {
+    ({ input, expected }) => {
       const { error, value } = Journal.joi(input);
 
       expect(error).toBeDefined();
