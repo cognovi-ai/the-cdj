6ab1af21e3681f23b2b582ed933cf41ff1fea12c
David Lu <ludavid33@gmail.com>
2024-11-05 17:21:33 -0800

    test: use memory mongodb replica set for tests
    Set up replica set with 3 nodes in jest.setup.cjs for delete tests

--- backend/tests/jest.teardown.cjs
+++ backend/tests/jest.teardown.cjs
@@ -3,6 +3,6 @@ require('dotenv').config();
 module.exports = async () => {
   console.log('Teardown: Dropping test database...');
   // https://typegoose.github.io/mongodb-memory-server/docs/guides/integration-examples/test-runners/
-  const instance = global.__MONGOINSTANCE;
-  await instance.stop();
+  const replSet = global.__MONGOREPLSET;
+  await replSet.stop();
 };
