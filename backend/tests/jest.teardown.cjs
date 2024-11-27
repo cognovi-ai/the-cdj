5ce1fa7b70c64ba4c2053fb5f23d2bc13298154a
David Lu <ludavid33@gmail.com>
2024-11-26 20:32:00 -0800

    refactor: update jest.teardown.cjs
    Integrate feedback from code review combining two lines into one.

--- backend/tests/jest.teardown.cjs
+++ backend/tests/jest.teardown.cjs
@@ -3,6 +3,6 @@ require('dotenv').config();
 module.exports = async () => {
   console.log('Teardown: Dropping test database...');
   // https://typegoose.github.io/mongodb-memory-server/docs/guides/integration-examples/test-runners/
-  const replSet = global.__MONGOREPLSET;
-  await replSet.stop();
+  // Stop the MongoMemoryReplSet instance assigned to global variable in jest.setup.cjs
+  await global.__MONGOREPLSET.stop();
 };
