6ca3c1a1aff43bee7c6f9f481d4ee618c05cb385
Ryan James Meneses <github@ryanmeneses.com>
2025-01-16 11:29:39 -0800

    fix(db): ensure env vars are retrieved in their specific environments
    - Env vars differ per environment.
      - `production` uses `ATLAS_URI`.
      - `development` uses `MONGO_URI`.
    
    - Server error prevented app from starting.
      - Due to `MONGO_URI` not being defined in `production`.
      - Resolve by moving retrieval of env vars inside env specific conditionals.

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,29 +1,31 @@
 import { connect } from 'mongoose';
 
 export default async function connectDB(dbName: string): Promise<void> {
-  // Define the local URI for development and testing
-  const localUri = `${process.env.MONGO_URI}/${dbName}`;
-
-  // Define the MongoDB Atlas URI for production
-  const atlasUri = process.env.ATLAS_URI;
-
   try {
     if (process.env.NODE_ENV === 'production') {
+      // Define the MongoDB Atlas URI for production
+      const atlasUri = process.env.ATLAS_URI;
+      
       // Connect to MongoDB Atlas in production
       if (!atlasUri) {
         throw new Error(
           'ATLAS_URI is not defined in the environment variables'
         );
       }
+
       await connect(atlasUri);
       console.log('Connected to MongoDB Atlas');
     } else if (process.env.NODE_ENV !== 'test') {
+      // Define the local URI for development and testing
+      const localUri = `${process.env.MONGO_URI}/${dbName}`;
+
       // Connect to the local MongoDB for development
-      if (!process.env.MONGO_URI) {
+      if (localUri) {
         throw new Error(
           'MONGO_URI is not defined in the environment variables'
         );
       }
+
       await connect(localUri);
       console.log(`Connected to local MongoDB: ${localUri}`);
     } else {
